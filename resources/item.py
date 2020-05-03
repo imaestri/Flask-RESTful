ab2a124e8c9fe0bfef85b43c63734e37a9a612bb
Isaque Maestri <isaquemaestri01@gmail.com>
2020-05-03 19:19:57 -0300

    final commit SQLAlchemy 👏👏

--- resources/item.py
+++ resources/item.py
@@ -1,174 +1,65 @@
-import sqlite3
 from flask_restful import Resource, reqparse
-from  flask_jwt import jwt_required
-<<<<<<< HEAD
-=======
+from flask_jwt import jwt_required
 from models.item import ItemModel
->>>>>>> e6bc6ca... commit
+
 
 class Item(Resource):
-    
     parser = reqparse.RequestParser()
     parser.add_argument('price',
-        type=float,
-        required=True,
-        help="This field cannot be left blank!!"
-    )
-    
-    
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('store_id',
+                        type=int,
+                        required=True,
+                        help="Every item needs a store_id."
+                        )
+
     @jwt_required()
     def get(self, name):
-<<<<<<< HEAD
-        item  = self.find_by_name(name)
-        if item:
-            return item
-        return {'message': 'Item not found'}, 404
-    
-    @classmethod
-    def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-        
-        if row: 
-            return {'item': {'name': row[0], 'price': row[1]}}
-                
-    def post(self, name):
-        if self.find_by_name(name):
-=======
-        item  = ItemModel.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         if item:
             return item.json()
         return {'message': 'Item not found'}, 404
-    
-  
-                
+
     def post(self, name):
         if ItemModel.find_by_name(name):
->>>>>>> e6bc6ca... commit
-            return {'Message': "An item with name '{}' already exists.".format(name)}, 400
-        
-        data = Item.parser.parse_args()
-        
-<<<<<<< HEAD
-        item = {'name': name, 'price': data['price']}
-        
-        try:
-            self.insert(item)
-        except:
-            return{"message": "An error ocurred inserting the item."}, 500
-        
-        return item, 201  
-    
-    @classmethod
-    def insert(cls, item):
-        
-        connection  = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (item['name'], item['price']))
-        
-        connection.commit()
-        connection.close()
-        
-        
-        
-=======
-        item = ItemModel(name, data['price'])
-        
-        try:
-            item.insert() 
-        except:
-            return{"message": "An error ocurred inserting the item."}, 500
-        
-        return item.json(), 201  
-     
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
->>>>>>> e6bc6ca... commit
+        data = Item.parser.parse_args()
+
+        item = ItemModel(name, **data)
+
+        try:
+            item.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the item."}, 500
+
+        return item.json(), 201
+
     def delete(self, name):
-        
-        connection  = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "DELETE FROM items WHERE name = ?"
-        cursor.execute(query, (name,))
-        
-        connection.commit()
-        connection.close()
-        
-        return {'message': 'Item deleted'}
-      
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+            return {'message': 'Item deleted.'}
+        return {'message': 'Item not found.'}, 404
+
     def put(self, name):
         data = Item.parser.parse_args()
-        
-<<<<<<< HEAD
-        item = self.find_by_name(name)
-        updated_item = {'name': name, 'price': data['price']}
-        
-        if item is None:
-            try:
-                Item.insert(updated_item)
-=======
+
         item = ItemModel.find_by_name(name)
-        updated_item = ItemModel(name, data['price'])
-        
-        if item is None:
-            try:
-               updated_item.insert()
->>>>>>> e6bc6ca... commit
-            except:
-                return {"message": "An error occurred inserting the item."}, 500
+
+        if item:
+            item.price = data['price']
         else:
-            try:
-<<<<<<< HEAD
-                Item.update(updated_item)
-            except:
-                return {"message": "An error occurred updating the item."}, 500
-        
-        return updated_item
-    
-    @classmethod
-    def update(cls, item):
-        
-        connection  = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "UPDATE items SET price=? WHERE name=? "
-        cursor.execute(query, (item['price'], item['name']))
-        
-        connection.commit()
-        connection.close()
-        
-        
-=======
-               updated_item.update()
-            except:
-                return {"message": "An error occurred updating the item."}, 500
-        
-        return updated_item.json()
-    
->>>>>>> e6bc6ca... commit
+            item = ItemModel(name, **data)
+
+        item.save_to_db()
+
+        return item.json()
+
 
 class ItemList(Resource):
-    
     def get(self):
-        connection  = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "SELECT * FROM items "
-        result = cursor.execute(query)
-
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-    
-        connection.close()
-        
-        return {'items': items}
-    
-    
+        return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
--- resources/user.py
+++ resources/user.py
@@ -1,91 +1,27 @@
-import sqlite3
 from flask_restful import Resource, reqparse
-<<<<<<< HEAD
-
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id
-        self.username = username
-        self.password = password
-        
-        
-    @classmethod
-    def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-        
-        connection.close()
-        return user
-    
-    
-    @classmethod
-    def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-        
-        connection.close()
-        return user
-=======
 from models.user import UserModel
 
->>>>>>> e6bc6ca... commit
-    
+
 class UserRegister(Resource):
-    
     parser = reqparse.RequestParser()
     parser.add_argument('username',
-        type = str,
-        required = True,
-        help = "This field cannot be blank."
-        )
-    
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
     parser.add_argument('password',
-        type = str,
-        required = True,
-        help = "This field cannot be blank."
-        )
-    
-    
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
+
     def post(self):
         data = UserRegister.parser.parse_args()
-        
-<<<<<<< HEAD
-        if User.find_by_username(data['username']):
-=======
+
         if UserModel.find_by_username(data['username']):
->>>>>>> e6bc6ca... commit
-            return {"message": "A user with tha username already exists"}, 400
-        
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "INSERT INTO users VALUES (NULL, ?, ?)" 
-        cursor.execute(query, (data['username'], data['password']))
-        
-        connection.commit()
-        connection.close()
+            return {"message": "A user with that username already exists"}, 400
+
+        user = UserModel(data['username'], data['password'])
+        user.save_to_db()
 
         return {"message": "User created successfully."}, 201
-                
-                
-
-<<<<<<< HEAD
-=======
- 
->>>>>>> e6bc6ca... commit
